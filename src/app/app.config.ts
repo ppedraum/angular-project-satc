bc2ae72514420d0b2c1fd625555a0550616482ea
joseMarciano <marcianojosepaulo@gmail.com>
2025-05-02 14:59:53 -0300

    :wrench: setup tailwindcss, primeng, primeflex primeicons

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,21 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import { MessageService } from 'primeng/api';
+import Aura from '@primeng/themes/aura';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true })]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+        options: {
+          darkModeSelector: 'none',
+        },
+      },
+    }),
+    MessageService
+  ]
 };
