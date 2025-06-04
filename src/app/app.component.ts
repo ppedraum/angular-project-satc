ea139e8e94c16d5d368ece2809e769bec8b55636
Pedro <pedrovirtuozo@gmail.com>
2025-06-03 21:48:04 -0300

    asdgere

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { RouterOutlet } from '@angular/router';
+import { ToastModule } from 'primeng/toast';
 
 @Component({
   selector: 'app-root',
-  imports: [ToolbarComponent, RouterOutlet],
+  imports: [ToolbarComponent, RouterOutlet, ToastModule],
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
--- src/app/components/add/add.component.ts
+++ src/app/components/add/add.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CardModule } from 'primeng/card';
 import { FloatLabelModule } from 'primeng/floatlabel';
 import { InputText } from 'primeng/inputtext';
@@ -6,6 +6,7 @@ import { SelectModule } from 'primeng/select';
 import { ButtonModule } from 'primeng/button';
 import { signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-add',
@@ -24,6 +25,7 @@ export class AddComponent {
   types = signal(['Trabalho', 'Estudo', 'Saúde']);
   description = signal('');
   type = signal<any>(null);
+  messageService = inject(MessageService);
 
   isButtonDisabled() {
     return !this.description() || !this.type;
@@ -39,5 +41,11 @@ export class AddComponent {
 
     this.type.set(null);
     this.description.set('');
+
+    this.messageService.add({
+      severity: 'success',
+      summary: 'Salvo',
+      detail: 'Salvo cm sucesso',
+    });
   }
 }
